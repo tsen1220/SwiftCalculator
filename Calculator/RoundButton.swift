11691e9c0703103cee7057ad1aa4636db042d25e
tseng1220 <tsen1220.us@gmail.com>
2019-12-24 21:18:12 +0800

    Layout

---
+++ Calculator/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  Calculator
+//
+//  Created by 曾子恩 on 2019/12/24.
+//  Copyright © 2019 曾子恩. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
